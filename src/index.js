b8a7392f51296acb7c0a8f2773deb8c820f386c6
Erica <ericaperry@quickenloans.com>
2020-11-02 17:51:41 -0500

    add: configuration to connect to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router } from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./index.css"
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
 
-import App from './components/App'
+const store = configureStore();
 
-render(<Router><App /></Router>, document.getElementById("app"));
+import App from "./components/App";
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("app")
+);
